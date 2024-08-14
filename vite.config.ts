7f7838526f978e97d8456e4e321b20b1b62a22b6
ajwtf <ajwtf@icloud.com>
2024-08-14 11:33:19 -0500

    updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,13 +5,4 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-
-  define: {
-    'process.env': {
-      NODE_ENV: JSON.stringify('production'),
-    },
-  },
-  build: {
-    sourcemap: true, // Recommended for debugging GA events
-  },
 });
